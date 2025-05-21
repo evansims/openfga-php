8f0d71029ca0ecb3b2515c7999e68778c5fa775b
Evan Sims <hello@evansims.com>
2025-05-21 04:07:47 -0500

    feat(tools): enhance documentation generator

---
+++ tests/Unit/Docs/DocumentationGeneratorTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+require_once implode(DIRECTORY_SEPARATOR, [__DIR__, '..', '..', '..', 'tools', 'docs', 'generate-docs.php']);
+
+use ReflectionClass;
+
+test('method signature prints default values using PHP syntax', function (): void {
+    class Dummy {
+        public function foo(string $bar = 'baz', array $data = [1, 2]): void {}
+    }
+
+    $generator = new DocumentationGenerator(__DIR__, __DIR__);
+    $method = (new ReflectionClass(Dummy::class))->getMethod('foo');
+
+    $signatureMethod = (new ReflectionClass(DocumentationGenerator::class))
+        ->getMethod('getMethodSignature');
+    $signatureMethod->setAccessible(true);
+    $signature = $signatureMethod->invoke($generator, $method);
+
+    expect($signature)
+        ->toContain("= 'baz'")
+        ->and($signature)->toContain('array')
+        ->and($signature)->toContain('0 => 1');
+});
