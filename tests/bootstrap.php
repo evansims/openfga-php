5785d22e5d7772e5fb2611e2d2ea3852146ca428
Evan Sims <hello@evansims.com>
2025-06-13 23:20:15 -0500

    chore: linter fixes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 /**
  * Test bootstrap file to load helper functions without polluting global autoload.
- * 
+ *
  * This file is loaded only during testing and includes the Helpers.php file
  * to make helper functions like tuple(), tuples(), etc. available to tests
  * without adding them to the main composer.json autoload section.
